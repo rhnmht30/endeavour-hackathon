86392759c22c999cdaf2c871a7d1e61a656cee50
dsrivastava565 <dsrivastava565@gmail.com>
2019-05-06 15:21:58 +0530

    reconstruct

--- Server/login/views.py
+++ Server/login/views.py
@@ -25,13 +25,7 @@ def login(request):
                 if user:
                     data_values = {'message': "Success", 'uniq_id': query[0]['uniq_id']}
                     return JsonResponse(data=data_values, safe=False)
-                elif username:
-                    if data['password'] == 'KIET123':
-                        return JsonResponse({'message': 'successfuly login'}, safe=False)
-                    else:
-                        return JsonResponse({'message': 'wrong password'}, safe=False)
-                else:
-                   return JsonResponse({'message': 'wrong username or password'}, safe=False)
+                
         else:
             return JsonResponse({'message': 'wrong username or password'}, safe=False)
     else:
@@ -51,7 +45,7 @@ def register(request):
             return JsonResponse({'message':'username Existed'},safe =False,status=201)
         else:
             query = MyUser.objects.create(name=data['name'],username=data['username'], password=data['password'],email=data['email'],gender=data['gender'],mobile=data['mobile'])
-            return JsonResponse({'message': 'Ssuccess'}, safe=False, status=201)
+            return JsonResponse({'message': 'successfully Inserted'}, safe=False, status=201)
 
 
 def addCar(request):
@@ -62,26 +56,26 @@ def addCar(request):
 
         # carnumber = CarDetails.object
         if user:
-            query= CarDetails.objects.create(carname=data['carname'],carseats=data['carseats'],addedBy=user,carNumber =data['carNumber'] )
+            query= CarDetails.objects.create(carname=data['carname'],carseats=data['carseats'],addedBy=user,carNumber =data['carnumber'] )
             if query:
                 return JsonResponse({'message': 'Success'}, safe=False, status=201)
             else:
                 return JsonResponse({'message': 'some error occured'}, safe=False, status=400)
         else:
-            return JsonResponse({'message': 'some error occured'}, safe=False, status=400)
+            return JsonResponse({'message': 'User Doesnot Exist'}, safe=False, status=400)
 
 
 def addPool(request):
     if request.method == 'POST':
         data = json.loads(request.body.decode("utf-8"))
         carnumber = CarDetails.objects.filter(carNumber=data['carNumber']).values('cardetailsId')
-        if list(carnumber):
-            print(carnumber[0].get('cardetailsId'))
-            myCar = CarDetails.objects.get(cardetailsId=carnumber[0].get('cardetailsId'))
-            seat = list(CarDetails.objects.filter(carNumber=data['carNumber']).values('carseats'))
-            print(seat)
+        if carnumber:
+            print(carnumber.get('cardetailsId'))
+            myCar = CarDetails.objects.get(cardetailsId=carnumber.get('cardetailsId'))
+            # seat = list(CarDetails.objects.filter(carNumber=data['carNumber']).values('carseats'))
+            # print(seat)
             if myCar:
-                query = startPooling.objects.create(time_start=data['time_start'],time_end = data['time_end'],latitude=data['latitude'],longitude=data['longitude'],status='ACTIVE',carPooled=myCar,seats=seat[0].get('carseats'))
+                query = startPooling.objects.create(time_start=data['time_start'],time_end = data['time_end'],latitude=data['latitude'],longitude=data['longitude'],status='ACTIVE',carPooled=myCar,seats=data['carseats'])
                 query2 = list(startPooling.objects.filter(carPooled=myCar).values('latitude','longitude'))
                 print(query2)
                 return JsonResponse({'message': 'Success'}, safe=False, status=201)
@@ -105,16 +99,15 @@ def getPoolDetails(request):
     pools = []
     if request.method=='GET':
         query = list(startPooling.objects.filter(status="ACTIVE").values('time_start','time_end','latitude','longitude','carPooled_id'))
-        print(query)
+        # print(query)
         for q in query:
-
             car=q.get('carPooled_id')
             cardeatils = list(CarDetails.objects.filter(cardetailsId=car).values('carname','carseats','carNumber','addedBy'))
             # pools.append({'pooling': q,'cardetails':list(cardeatils)})
             for m in cardeatils:
                 userdetails = list(MyUser.objects.filter(uniq_id=cardeatils[0].get('addedBy')).values('name','mobile','email'))
                 for x in userdetails:
-                    pools.append({'pooling': q, 'cardetails': m,'userdetails':x})
+                    pools.append({'pooling': q, 'cardetails': m,'poolerdetails':x})
                     print(pools)
             # print(q.get('carPooled_id'))
             # print(carpooled)
@@ -123,22 +116,38 @@ def getPoolDetails(request):
         return JsonResponse({'message': 'errro'}, safe=False, status=500)
 
 def addUsertoPool(request):
-    if request.method=='GET':
-        query = list(startPooling.objects.filter(carPooled=request.GET['carPooled_id']).values('seats'))
-        seat1 = int(query[0].get('seats'))
-        if seat1>0 :
-            print(seat1)
-            user= MyUser.objects.get(uniq_id=request.GET['user_id'])
-            poolingwithid = startPooling.objects.get(carPooled=request.GET['carPooled_id'])
-            queryforuser = userPooling.objects.create(userPooled=user,status='ACTIVE',poolingwithid=poolingwithid)
-            newquery= startPooling.objects.filter(carPooled=request.GET['carPooled_id']).update(seats = seat1-1)
-            print(newquery)
-            return JsonResponse({'message': 'Successfully Pooled'}, safe=False, status=200)
+    if request.method=='POST':
+        data = json.loads(request.body.decode("utf-8"))
+        print(data)
+        uniq = data['uniq_id']
+        query_search = list(MyUser.objects.filter(uniq_id=uniq).values('uniq_id'))
+        # print(query_search.get('uniq_id'))
+        if query_search:
+            query_car_id = list(CarDetails.objects.filter(carNumber=data['carNumber']).values('cardetailsId'))
+            if query_car_id:
+                query_search_seats = list(startPooling.objects.filter(carPooled=query_car_id[0].get('cardetailsId')).values('seats','startPoolingId'))
+                print(query_search_seats[0].get('seats'))
+                if query_search_seats[0].get('seats')>0:
+                    try:
+                        search_already_pooled_once = userPooling.objects.filter(userPooled = data['uniq_id'] ).values('status')
+                        if search_already_pooled_once[0].get('status') == 'ACTIVE':
+                            return  JsonResponse({'message': 'User Already Pooled'}, safe=False, status=400)
+                        elif search_already_pooled_once[0].get('status') == 'INACTIVE':
+                            poolingwith = startPooling.objects.get(carPooled=query_car_id[0].get('cardetailsId'))
+                            print("checkkkkk")
+                            userPooling.objects.filter(userPooled=data['uniq_id']).update(status = 'ACTIVE',poolingwithid=poolingwith)
+                            return  JsonResponse({'message': 'User Successfully Updated'}, safe=False, status=200)
+                    except:
+                        add = MyUser.objects.get(uniq_id=uniq)
+                        poolingwith = startPooling.objects.get(carPooled=query_car_id[0].get('cardetailsId'))
+                        userPooling.objects.create(status='ACTIVE',userPooled=add,poolingwithid = poolingwith)
+                        return  JsonResponse({'message': 'User Successfully Pooled'}, safe=False, status=200)
+                    
+                return JsonResponse({'message': 'Seats Fulll'}, safe=False, status=400)
         else:
-            newquery = startPooling.objects.filter(carPooled=request.GET['carPooled_id']).update(status='INACTIVE')
-            return JsonResponse({'message': 'No seat Available'}, safe=False, status=200)
+            return JsonResponse({'message': 'User Not found'}, safe=False, status=400)
     else:
-        return JsonResponse({'message': 'wronng method'}, safe=False, status=401)
+        return JsonResponse({'message': 'Wrong Request type'}, safe=False, status=400)
 
 def getcurrentPools(request):
     data_values = {}
